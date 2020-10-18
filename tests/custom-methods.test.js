5a9f5e01f74d660515acd0545b92f9d06512ed3d
notna <notnatrue@gmail.com>
2020-10-18 05:53:40 +0300

    add tests for custom request

--- tests/custom-methods.test.js
+++ tests/custom-methods.test.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 import "babel-polyfill";
 import "jest";
 import { ThirdPartyAPI } from "../src/third-party-api/leeloo/service";
+
 const thirdPartyApi = new ThirdPartyAPI();
 
 test('Gets Users from leeloo API', async () => {
@@ -26,3 +27,38 @@ test('Gets UserDetails from leeloo API', async () => {
         throw new Error(err);
     }
 })
+
+test("Testing custom request for Get Users from leeloo API", async () => {
+    try {
+        const options = {
+            url: 'https://api.stage.leeloo.ai/api/v1/accounts?limit=50&offset=0',
+            headers: {
+            'X-Leeloo-AuthToken': process.env.AUTH_TOKEN,
+            }
+        };
+        const response = await thirdPartyApi.customRequest(options).catch((err) => { throw new Error(err) });
+        expect(response).not.toBe(null);
+        expect(response).not.toBe(undefined);
+        expect(typeof response).toBe("object");
+    } catch(err) {
+        throw new Error(err);
+    }
+});
+
+test("Testing custom request for Get Users Details from leeloo API", async () => {
+    try {
+        const userId = "5e01e141ec4ffd000d3b5c1a";
+        const options = {
+            url: `https://api.stage.leeloo.ai/api/v1/accounts/${ userId }?include=contactedUsers,orders`,
+            headers: {
+                'X-Leeloo-AuthToken': process.env.AUTH_TOKEN,
+            }
+        };
+        const response = await thirdPartyApi.customRequest(options).catch((err) => { throw new Error(err) });
+        expect(response).not.toBe(null);
+        expect(response).not.toBe(undefined);
+        expect(typeof response).toBe("object");
+    } catch(err) {
+        throw new Error(err);
+    }
+});
